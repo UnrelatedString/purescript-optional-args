0efc7e92acdf8f5e788d02181a49a28edbe19b5d
UnrelatedString <unrelatedstring@gmail.com>
2025-03-30 16:44:08 -0400

    getting at the shape of it methinks

--- src/Record/Optional.purs
+++ src/Record/Optional.purs
@@ -5,8 +5,22 @@ module Record.Optional
   , type (-?->)
   ) where
 
-data Optional
-data WithDefaults
+import Prelude
+
+import Prim.Row as R
+import Prim.RowList as RL
+
+data Optional :: (Type -> Type) -> Type -> Type
+data Optional f a
+
+type WithDefaults a b = forall r. AdequatelySpecifies r a => r -> b
 
 infix 8 type Optional as ?
 infixr 4 type WithDefaults as -?->
+
+class AdequatelySpecifies x y
+
+class CompatibleArgument x y
+
+instance CompatibleArgument (Optional f a) (f a) else
+instance CompatibleArgument a a
