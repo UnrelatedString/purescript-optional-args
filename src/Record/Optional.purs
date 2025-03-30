89f752d671c3baf92c4ec0bb0dc19b8e168c949d
UnrelatedString <unrelatedstring@gmail.com>
2025-03-30 16:55:46 -0400

    bwaaaaa why don't backticks work for types ;_;

--- src/Record/Optional.purs
+++ src/Record/Optional.purs
@@ -3,12 +3,15 @@ module Record.Optional
   , type (?)
   , WithDefaults
   , type (-?->)
+  , class AdequatelySpecifies
+  , class CompatibleArgument
   ) where
 
 import Prelude
 
 import Prim.Row as R
 import Prim.RowList as RL
+import Prim.TypeError as TE
 
 data Optional :: (Type -> Type) -> Type -> Type
 data Optional f a
@@ -18,9 +21,22 @@ type WithDefaults a b = forall r. AdequatelySpecifies r a => r -> b
 infix 8 type Optional as ?
 infixr 4 type WithDefaults as -?->
 
+-- | 
 class AdequatelySpecifies x y
 
+-- | Constrains two types to be the same or the first to be an Optional of the second.
 class CompatibleArgument x y
 
 instance CompatibleArgument (Optional f a) (f a) else
-instance CompatibleArgument a a
+instance CompatibleArgument a a else
+instance TE.Fail
+  ( TE.Text "Expected: "
+  `TE.Beside`
+    TE.Quote a
+  `TE.Beside`
+    TE.Text ", got: "
+  `TE.Beside`
+    TE.Quote b
+  `TE.Beside`
+    TE.Text "in a record spec with optional arguments"
+  ) => CompatibleArgument a b
