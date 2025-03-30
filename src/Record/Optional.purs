151c86dce91e617afee7963aa32596af3d38dfbf
UnrelatedString <unrelatedstring@gmail.com>
2025-03-30 17:16:20 -0400

    okay maybe a little uhh

--- src/Record/Optional.purs
+++ src/Record/Optional.purs
@@ -4,6 +4,8 @@ module Record.Optional
   , WithDefaults
   , type (-?->)
   , class AdequatelySpecifies
+  , specify
+  , class FullySpecified
   , class CompatibleArgument
   ) where
 
@@ -23,11 +25,15 @@ type WithDefaults a b = forall r. AdequatelySpecifies r a => r -> b
 infix 8 type Optional as ?
 infixr 4 type WithDefaults as -?->
 
--- | 
-class AdequatelySpecifies x y
+-- | Allows a record spec with `Optional`s to match real arguments.
+-- | Record specs are given as `Row`s (in parentheses) rather than `Record`s (in braces).
+class AdequatelySpecifies arg spec where
+  specify :: forall r. FullySpecified r spec => arg -> Record r
+
+class FullySpecified r spec
 
 -- | Constrains two types to be the same or the first to be an Optional of the second.
-class CompatibleArgument x y
+class CompatibleArgument x y | x -> y
 
 instance CompatibleArgument (Optional f a) (f a) else
 instance CompatibleArgument a a else
